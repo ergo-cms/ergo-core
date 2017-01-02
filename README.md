db1156f5c4a22c5259b0af3ac3dfdd0a2eaba1de
cmroanirgo <cmroanirgo@users.noreply.github.com>
2017-01-02 14:25:19 +1100

    travis prep

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 This comprises the complete set of functions used by plugins and the cli.
 
+
+[![npm Package](https://img.shields.io/david/ergo-cms/ergo-core.svg)](https://github.com/ergo-cms/ergo-core)
+[![build status](https://secure.travis-ci.org/ergo-cms/ergo-core.svg)](http://travis-ci.org/ergo-cms/ergo-core)
---
+++ test/init.js
@@ -0,0 +1,31 @@
+
+describe('Tests \'init\' functionality', function() {
+/*
+	var defaultOptions = {verbose:0,debug:false};
+
+	it('Verbosity is zero', function(){
+		assert.equal(log._verbosity(), 0);
+	});
+	it('Default options available', function() {
+		assert.isDefined(log._options)
+	})
+	it('All default options', function() {
+		assert.deepEqual(log._options, defaultOptions, "Default log options have changed")
+	});
+	it('Changes options', function() {
+		log.init({verbose:-100});
+		assert.equal(log._verbosity(),-100);
+	});
+	it('Checks default options changed', function() {
+		assert.notDeepEqual(log._options, defaultOptions);
+	});
+	it('Resets default options', function() {
+		log.init(defaultOptions); // reset
+		assert.equal(log._verbosity(),0);
+	});
+*/
+	it('No problems here', function(){
+		assert.equal(true, !!1);
+	});
+});
+
