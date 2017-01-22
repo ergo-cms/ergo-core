5d1c448f4ada714141a22ce7149abd9d62e57985
cmroanirgo <cmroanirgo@users.noreply.github.com>
2017-01-22 17:04:00 +1100

    updated init to work with skeleton repo

--- api/init.js
+++ api/init.js
@@ -15,15 +15,22 @@ var _  = require('ergo-utils')._;
 var fs = require('ergo-utils').fs.extend(require('fs-extra'));
 var path = require('path');
 var Promise = require('bluebird');
+var download = require("download-git-repo")
 
-module.exports = function(dir, skeleton_dir, options) {
+// promisify a few funcs we need
+"fileExists".split(',').forEach(function(fn) {
+	fs[fn] = Promise.promisify(fs[fn])
+});
+download = Promise.promisify(download);
+
+module.exports = function(dir, skeleton_repo, options) {
 	if (!_.isDefined(dir))
 		throw ('Command \'init\' is missing parameter \'dir\'');
-	if (!_.isDefined(skeleton_dir))
-		throw ('Command \'init\' is missing parameter \'skeleton_dir\'');
+	if (!_.isDefined(skeleton_repo))
+		throw ('Command \'init\' is missing parameter \'skeleton_repo\'');
 	options = options || {};
 
-	l.log('Initialising \'' + dir + '\' ...')
+	//l.log('Initialising \'' + dir + '\' ...')
 	return Promise.coroutine(function *() {
 		if (!options.force) {
 			// unless *forced* to, check that we have an empty dir first!
@@ -50,10 +57,15 @@ module.exports = function(dir, skeleton_dir, options) {
 			}
 		}
 
-		l.vlog('Copying skeleton files to destination...')
-		var copy = Promise.promisify(fs.copy);
-		yield copy(skeleton_dir, dir, {clobber:(options.force||false)});
-		return true;
+		yield download(skeleton_repo, dir, {});
+		if (!(yield fs.fileExists(path.join(dir, 'config.ergo.js')))) {
+			throw new Error("'"+skeleton_repo+"' is not a valid skeleton file. It is missing 'config.ergo.js'")
+		}
+
+
+//		var copy = Promise.promisify(fs.copy);
+//		yield copy(skeleton_dir, dir, {clobber:(options.force||false)});
+		return skeleton_repo;
 	})();
 }
 
