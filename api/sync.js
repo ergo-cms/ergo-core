9727c6f5c74e23e23e95ecef0cdac4a032941cac
cmroanirgo <cmroanirgo@users.noreply.github.com>
2017-02-15 18:51:55 +1100

    cleaned up sync api. Added deploy

--- api/sync.js
+++ api/sync.js
@@ -11,38 +11,67 @@
 
 var l  = require('ergo-utils').log.module('ergo-api-sync');
 var _  = require('ergo-utils')._;
-var fs = require('ergo-utils').fs.extend(require('fs-extra'));
 var path = require('path');
 var Promise = require('bluebird');
 var sync_helper = require('../lib/sync-helper')
 
-// promisify a few funcs we need
-"dirExists,fileExists,ensureDir,emptyDir,readFile,writeFile,readdir,remove".split(',').forEach(function(fn) {
-	fs[fn] = Promise.promisify(fs[fn])
-});
 
 
 function _sync(options) {
 return Promise.coroutine(function *() {
 	options = options || {};
 	var context = options.context || (yield require('./config').getContextP(options.working_dir));
-	if (!context.config.sync)
-		throw new Error("Missing 'sync' settings in config.ergo.js")
+	if (!context.config.project_sync)
+		throw new Error("Missing 'project_sync' settings in config.ergo.js")
 
-	var server = _.extend({}, context.config.sync);
-	var ignore = [context.getRelOutPath(), sync_helper.SYNC_FILE];
-	server.force = options.force;
-	switch (server.type || 'ssh') {
+	var connection = _.extend({}, context.config.project_sync);
+	var syncOptions = {
+		//direction: sync_helper.UPLOAD,
+		direction: undefined, // === Bi-directional
+		ignore: [context.getRelOutPath(), sync_helper.SYNC_FILE],
+		force: options.force,
+		connection: connection,
+	}
+	switch (connection.type || 'ssh') {
 		case 'ssh':
-			return require('../lib/ssh-sync')(context.getBasePath(), server.path, server, ignore);
+			return require('../lib/ssh-sync')(context.getBasePath(), connection.path, syncOptions);
 		default:
 			throw new Error("Unknown sync type: " + server.type);
 	}
 
 	return true;
-})();}
+})();
+}
+
+
+
+function _deploy(options) {
+return Promise.coroutine(function *() {
+	options = options || {};
+	var context = options.context || (yield require('./config').getContextP(options.working_dir));
+	if (!context.config.deploy)
+		throw new Error("Missing 'deploy' settings in config.ergo.js")
+
+	var connection = _.extend({}, context.config.deploy);
+	var syncOptions = {
+		direction: sync_helper.UPLOAD,
+		ignore: [sync_helper.SYNC_FILE],
+		force: options.force,
+		connection: connection,
+	}
+	switch (connection.type || 'ssh') {
+		case 'ssh':
+			return require('../lib/ssh-sync')(context.getOutPath(), connection.path, syncOptions);
+		default:
+			throw new Error("Unknown sync type: " + server.type);
+	}
+
+	return true;
+})();
+}
 
 
 module.exports = {
 	sync: _sync,
+	deploy: _deploy,
 }
